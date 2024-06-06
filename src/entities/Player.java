a4968a7881bf4bba4c32f01a4596c8344b64a30c
ldg skkusosc <83210475+DongGyunLeeeee@users.noreply.github.com>
2024-06-06 13:03:29 +0900

    Update Player.java

---
+++ src/entities/Player.java
@@ -0,0 +1,22 @@
+import java.awt.Image;
+
+public class Player extends Sprite {
+	private Game game;
+
+	public Player (Game game, Image image, int x, int y) {
+		super(image, x, y);
+		this.game = game;
+		dx = 0;
+    dy = 0;
+	}
+
+	public void move() {
+    if ((dx < 0) && (x < 10)) {
+			return;
+		}
+		if ((dx > 0) && (x > 800)) {
+			return;
+		}
+		super.move();
+	}
+}
