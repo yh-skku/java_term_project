e9c836b3eb0f9d14c627906c78cf91877db7ebe9
ldg skkusosc <83210475+DongGyunLeeeee@users.noreply.github.com>
2024-06-06 16:07:53 +0900

    Update Bullet.java

--- src/entities/Bullet.java
+++ src/entities/Bullet.java
@@ -1,26 +1,25 @@
 import java.awt.Image;
 
 public class Bullet extends Sprite {
-	private Game game;
-
-	public Bullet (Game game, Image image, int x, int y) {
+	private SpriteMager sm;
+	
+	public Bullet (SpriteManager sm, Image image, int x, int y) {
 		super(image, x, y);
-		this.game = game;
+		this.sm = sm;
 		dy = -5;
 	}
 
 	public void move() {
 		super.move();  
 		if (y < -100) { 
-			game.removeSprite(this); 
+			sm.removeSprite(this); 
 		}
 	}
 
 	public void handleCollision(Sprite other) {
 		if (other instanceof Enemy) {
-			game.removeSprite(other);
-			game.removeSprite(this);
-			game.score += 10;
+			sm.removeSprite(other);
+			sm.removeSprite(this);
 		}
 	}
 }
