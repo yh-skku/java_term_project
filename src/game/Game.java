aa9d0651b5b112a0de08270934ae6df4364452ab
yh_skku <yh03061@g.skku.edu>
2024-06-06 12:10:36 +0900

    add images and modify game.java main.java

---
+++ src/game/Game.java
@@ -0,0 +1,80 @@
+package game;
+
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+    private boolean running = false;
+    private Thread thread;
+
+    public Game() {
+        JFrame frame = new JFrame("Galaga Game");
+        frame.setSize(GameSettings.WIDTH, GameSettings.HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(this);
+        frame.setVisible(true);
+        this.addKeyListener(new InputHandler());
+    }
+
+    public synchronized void start() {
+        if (running) return;
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        if (!running) return;
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        this.requestFocus();
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                tick();
+                delta--;
+            }
+            if (running) render();
+            frames++;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void tick() {
+        // 게임 로직 업데이트
+    }
+
+    private void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+        // 화면 그리기
+        g.dispose();
+        bs.show();
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+import game.Game;
+
+public class Main{
+    public static void Main(String[] args){
+        Game game = new Game();
+        game.start();
+    }
+
+}
